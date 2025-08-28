20b9a5df04958cf32c6820697b5efb670e228776
Heechan Park <phcphc82@gmail.com>
2025-08-29 05:26:04 +0900

    도서관 리스트를 data table로 변경

--- app/features/library/pages/library-page.tsx
+++ app/features/library/pages/library-page.tsx
@@ -1,49 +1,42 @@
-import type { Route } from "./+types/library-page";
+import type { ColumnDef } from "@tanstack/react-table";
 
-import { Link } from "react-router";
+import type { Route } from "./+types/library-page";
 
 import { Hero } from "~/common/components/hero";
 import { Button } from "~/common/components/ui/button";
 import { Checkbox } from "~/common/components/ui/checkbox";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/common/components/ui/table";
+
+import { DataTable } from "../components/data-table";
+
+type Library = {
+  id: string;
+  name: string;
+  url: string;
+  group: string;
+};
 
 export const loader = ({ params }: Route.LoaderArgs) => {
-  const libraries = [
+  const libraries: Library[] = [
     {
-      id: "1",
-      checked: true,
+      id: "m5gr84i9",
       name: "서울시교육청 동작도서관",
       url: "https://djlib.sen.go.kr/",
-      digital: false,
-      groupId: "1",
       group: "서울시교육청",
     },
     {
-      id: "2",
-      checked: true,
+      id: "3u1reuv4",
       name: "서초구립 반포도서관",
       url: "https://www.seocholib.or.kr/",
-      digital: false,
-      groupId: "2",
       group: "서초구립",
     },
     {
-      id: "3",
-      checked: false,
+      id: "derv1ws0",
       name: "사당솔밭도서관",
       url: "https://lib.dongjak.go.kr/",
-      digital: false,
-      groupId: "3",
       group: "동작구",
     },
   ];
+
   return { libraries: libraries };
 };
 
@@ -55,38 +48,50 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function LibraryPage({ loaderData }: Route.ComponentProps) {
-  //console.log(loaderData.libraries);
+  const columns: ColumnDef<Library>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "name",
+      header: "이름",
+      cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+    },
+    {
+      accessorKey: "url",
+      header: "홈페이지",
+      cell: ({ row }) => <div className="capitalize">{row.getValue("url")}</div>,
+    },
+    {
+      accessorKey: "group",
+      header: "그룹",
+      cell: ({ row }) => <div className="capitalize">{row.getValue("group")}</div>,
+    },
+  ];
+
   return (
     <div className="space-y-10">
       <Hero title="도서관 리스트" subtitle="검색할 도서관을 선택하세요." />
-      <div className="">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>
-                <Checkbox />
-              </TableHead>
-              <TableHead>이름</TableHead>
-              <TableHead>홈페이지</TableHead>
-              <TableHead>그룹</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {loaderData.libraries.map((library) => (
-              <TableRow>
-                <TableCell>
-                  <Checkbox />
-                </TableCell>
-                <TableCell>{library.name}</TableCell>
-                <TableCell>
-                  <Link to={library.url}>{library.url}</Link>
-                </TableCell>
-                <TableCell>{library.group}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+      <DataTable columns={columns} data={loaderData.libraries} />
       <div className="flex justify-end space-x-2">
         <Button variant="outline">취소</Button>
         <Button>저장</Button>
