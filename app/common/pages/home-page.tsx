43febbd8e88516c816806063b5131b79e40f8de0
Heechan Park <phcphc82@gmail.com>
2025-09-01 04:09:35 +0900

    홈페이지 정리

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,9 +1,17 @@
+import { Hero } from "~/common/components/hero";
+
 export default function HomePage() {
   return (
-    <main className="container mx-auto px-4 py-8">
-      <h1 className="mp-6 text-4xl font-bold">
-        도서관<sup>+</sup>
-      </h1>
-    </main>
+    <div>
+      <Hero title="도서관⁺" subtitle="" />
+      <div className="flex flex-col items-center">
+        <div>간편한 도서관 도서 검색 서비스</div>
+        <div>한 번의 입력으로 여러 도서관을 동시에 검색</div>
+        <div>대출 가능 도서관이 한 눈에</div>
+        <div>중복 도서는 하나로</div>
+        <div>.</div>
+        <div>지금 가입하세요</div>
+      </div>
+    </div>
   );
 }
