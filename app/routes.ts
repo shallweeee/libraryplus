f8d6216d59bbce47e069d2d8c66865f3c9445b93
Heechan Park <phcphc82@gmail.com>
2025-09-01 02:06:19 +0900

    library redirect 페이지 추가

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,17 @@
-import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
+
+import { index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
   index("common/pages/home-page.tsx"),
   route("/search", "features/library/pages/search-page.tsx"),
-  route("/libraries", "features/library/pages/library-page.tsx"),
+  ...prefix("/libraries", [
+    index("features/library/pages/library-redirect-page.tsx"),
+    route("/list", "features/library/pages/library-page.tsx"),
+    /*
+    route("/request", "features/library/pages/library-request-page.tsx"),
+    */
+  ]),
   ...prefix("/auth", [
     layout("features/auth/layouts/auth-layout.tsx", [
       route("/login", "features/auth/pages/login-page.tsx"),
