be8f6e10304c45dc93507e4350a431516bd5205b
Heechan Park <phcphc82@gmail.com>
2025-09-28 13:44:17 +0900

    대출 정보 안내 문구 추가

--- app/features/books/components/book-card.tsx
+++ app/features/books/components/book-card.tsx
@@ -42,26 +42,32 @@ export function BookCard({ isbn, image, title, author, publisher, published }: B
         {fetcher.state !== "idle" && <CardFooter>확인중</CardFooter>}
         {fetcher.state === "idle" && libs && (
           <CardFooter>
-            <ul className="flex flex-col items-start">
-              {libs.map((lib) => (
-                <div className="flex gap-4">
-                  {lib.available ? (
-                    <Badge variant="secondary" className="w-20 text-center">
-                      대출 가능
-                    </Badge>
-                  ) : (
-                    <Badge variant="outline" className="w-20 text-center">
-                      대출 중
-                    </Badge>
-                  )}
-                  <Button variant="link" asChild>
-                    <a href={lib.homepage} target="_blank" rel="noopener noreferrer">
-                      {lib.lib_name}
-                    </a>
-                  </Button>
-                </div>
-              ))}
-            </ul>
+            <div className="flex flex-col gap-4">
+              <div>
+                안내: 대출 정보는 전날 데이터로 현재와 다를 수 있습니다. 실시간 정보로 변경
+                중입니다.
+              </div>
+              <ul className="flex flex-col items-start gap-1">
+                {libs.map((lib) => (
+                  <div className="flex gap-4">
+                    {lib.available ? (
+                      <Badge variant="secondary" className="w-20 text-center">
+                        대출 가능
+                      </Badge>
+                    ) : (
+                      <Badge variant="outline" className="w-20 text-center">
+                        대출 중
+                      </Badge>
+                    )}
+                    <Button variant="link" asChild>
+                      <a href={lib.homepage} target="_blank" rel="noopener noreferrer">
+                        {lib.lib_name}
+                      </a>
+                    </Button>
+                  </div>
+                ))}
+              </ul>
+            </div>
           </CardFooter>
         )}
       </Card>
